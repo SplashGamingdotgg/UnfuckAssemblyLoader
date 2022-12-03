f70d45f1b71319c285ac8a04d3b391be26e823fc
Diametric <russ@zerotech.net>
2022-12-03 14:40:38 -0500

    This is essentially a complete rewrite.
    This hooks into Rust's HarmonyLoader class to search for already loaded HarmonyMods referenced by // Reference, and then uses that information to find the correct DLL.

--- README.md
+++ README.md
@@ -1,5 +1,48 @@
 # UnfuckAssemblyLoader
 
-This should really be called Unfuck**Oxide**AssemblyLoader.  
+~~This should really be called Unfuck**Oxide**AssemblyLoader.~~  
 
-This allows DLLs in the Rust `HarmonyMods` folder to be used as references ( `// Reference: xyz.dll` ) in Oxide plugins. Without the ability to do this, it becomes very difficult to create combined Oxide and Harmony workflows.
+> tl;dr - Dec 2022 patch of Rust broke the ability for Oxide to reference any Harmony DLLs entirely.
+> This fixes that.
+
+I stand by the original name now.   As of December 1st 2022's patch, Rust's Harmony loader fully 
+supports loading and unloading at runtime.  However, the caveat to this was assembly namespaces 
+needed to be juked. This causes an issue where referencing assemblies of the same name, from 
+anywhere, including the HarmonyMod's folder loads them into a different namespace.  
+Unfortunately .NET's CLR isn't clever enough to allow casting between identical assemblies in 
+different namespaces. So it leaves all the code in a harmony mod assembly untouchable without 
+very complicated, and frustrating reflection.
+
+In light of these changes, I've updated the Harmony mod to attempt to load the already loaded 
+assembly from the HarmonyLoader class in Rust.  This is not without caveats.  As this time I 
+won't have written any dependency reload checking. So if you unload a Harmony DLL that is being 
+utilized by a plugin, expect bad things. **Unload the plugin first, then unload/reload your 
+Harmony DLL.**
+
+
+# FAQ
+
+- [How do I use it?](#how-do-i-use-it)
+- [Why not just submit a PR for native Oxide support?](#no-pr-for-oxide)
+- [Why would you even want this functionality?](#why-use-it)
+
+## How do I use it?
+
+Download the latest release from the releases page, and place it in your `HarmonyMods` folder.
+Use `// Reference: MyHarmonyMod` in your plugin to reference the harmony mod assembly.
+
+## Why not just submit a PR for native Oxide support?
+
+The code we're modifying here is the `Oxide.CSharp` module.  This is a game agnostic module,
+so it complicates trying to support a Rust specific feature/fix. 
+
+## Why would you even want this functionality?
+
+There are a lot of reasons you'd want to load DLLs into Oxide plugins. In my specific case I 
+wrote a Harmony mod that tracks various PVP statistics.  I wanted to use it on a non-oxide, 
+vanilla only server for external support (such as posting stats to discord, websites, etc.)
+
+I also wanted to use it on a modded server and integrate it with a killfeed. I created some
+simply callback hooks that would allow me to hook into the stats mod to get the data needed
+to display the killfeed using an Oxide plugin and CUI.  There are countless other use cases
+for this functionality, and I hope this helps someone else out there.
--- UnfuckAssemblyLoader/UnfuckAssemblyLoader.cs
+++ UnfuckAssemblyLoader/UnfuckAssemblyLoader.cs
@@ -1,161 +1,162 @@
 ﻿// Author: Diametric
-// Lets you use "// Reference:" in Oxide mods and load assemblies from the HarmonyMods folder.
-// Caveats: Prefers HarmonyMods first, so if Blah.dll exists in both HarmonyMods and Managed folders, the HarmonyMods version will be used.
+// Copyright (c) Splash Gaming
+// Lets you use "// Reference:" in Oxide mods and uses the assembly loaded by Harmony.
 
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Reflection.Emit;
 using Harmony;
+using Oxide.Plugins;
 using UnityEngine;
 
 namespace UnfuckAssemblyLoader
 {
     [HarmonyPatch]
-    internal class UnfuckFirstAddReference : AbstractPatchyBoi
+    internal class UnfuckAddReference
     {
-        private static readonly CodeInstruction[] Search =
-        {
-            new CodeInstruction(OpCodes.Call, typeof(Oxide.Core.Interface).GetProperty("ExtensionDirectory")),
-            new CodeInstruction(OpCodes.Callvirt, typeof(Oxide.Core.Interface).GetProperty("Oxide")),
-            new CodeInstruction(OpCodes.Ldarg_2),
-            new CodeInstruction(OpCodes.Ldstr, ".dll"),
-            new CodeInstruction(OpCodes.Call, typeof(string).GetMethod("Concat", new[] { typeof(string), typeof(string) })),
-            new CodeInstruction(OpCodes.Call, typeof(Path).GetMethod("Combine", new[] { typeof(string), typeof(string) })),
-            new CodeInstruction(OpCodes.Call, typeof(File).GetMethod("Exists"))
-        };
-
-        private static readonly CodeInstruction[] Patch =
-        {
-            new CodeInstruction(OpCodes.Ldarg_2),
-            new CodeInstruction(OpCodes.Call, typeof(UnfuckFirstAddReference).GetMethod("AssemblyExists"))
-        };
-
-        public static bool AssemblyExists(string assemblyName)
-        {
-            return File.Exists(Path.Combine(Oxide.Core.Interface.Oxide.ExtensionDirectory, assemblyName + ".dll")) ||
-                   File.Exists(Path.Combine(Oxide.Core.Interface.Oxide.RootDirectory, "HarmonyMods", assemblyName + ".dll"));
-        }        
+        // Internal class Type for the Compilation class. The irony of making a class harder to mod in a framework
+        // built to mod games.
+        private static Type _oxideCompilationClass = null;
+        private static Type _oxideCompilerFile = null;
+        private static MethodInfo _internalAddReferenceMethod = null;
+        private static Dictionary<string, Assembly> _harmonyLoaderAssemblies = null;
         
-        static MethodInfo TargetMethod()
+        private static Assembly GetHarmonyAssembly(string name)
         {
-            // Needed to do this, the internal class couldn't be found even with a fully qualified string to Type.GetType (i dunno why)
-            var assembly = Assembly.GetAssembly(typeof(Oxide.Plugins.Timer));
-            var type = assembly.GetType("Oxide.Plugins.Compilation");
-            var method = type.GetMethod(
-                "AddReference",
-                BindingFlags.Instance | BindingFlags.NonPublic,
-                null,
-                new Type[] {typeof(Oxide.Plugins.CompilablePlugin), typeof(string)},
-                null);
-            
-            return method;
-        }
-
-        [HarmonyTranspiler]
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            return ApplyPatch(instructions, Search, Patch);
-        }
-    }
-
-    [HarmonyPatch]
-    internal class UnfuckSecondAddReference : AbstractPatchyBoi
-    {
-        private static readonly CodeInstruction[] Search =
-        {
-            new CodeInstruction(OpCodes.Call, typeof(Oxide.Core.Interface).GetProperty("ExtensionDirectory")),
-            new CodeInstruction(OpCodes.Callvirt, typeof(Oxide.Core.Interface).GetProperty("Oxide"))
-        };
-
-        private static readonly CodeInstruction[] Patch =
-        {
-            new CodeInstruction(OpCodes.Ldloc_0),
-            new CodeInstruction(OpCodes.Call, typeof(UnfuckSecondAddReference).GetMethod("GetAssemblyDirectory"))
-        };
-
-        // Prefer HarmonyMods first. This doesn't really handle the case well if the assembly with the same name exists
-        // in both the extensions folder and harmonymods.
-        public static string GetAssemblyDirectory(string assemblyName)
-        {
-            if (File.Exists(Path.Combine(Oxide.Core.Interface.Oxide.RootDirectory, "HarmonyMods", assemblyName)))
+            foreach (var assembly in _harmonyLoaderAssemblies)
             {
-                return Path.Combine(Oxide.Core.Interface.Oxide.RootDirectory, "HarmonyMods");
+                Debug.Log($"[UnfuckAssemblyLoader] Checking {assembly.Key} for {name}");
             }
             
-            return Oxide.Core.Interface.Oxide.ExtensionDirectory;
+            Assembly asm;
+            return _harmonyLoaderAssemblies.TryGetValue(name, out asm) ? asm : null;
         }
 
-        static MethodInfo TargetMethod()
+        private static string GetJukedAssemblyName(string name)
         {
-            // Needed to do this, the internal class couldn't be found even with a fully qualified string to Type.GetType (i dunno why)
-            var assembly = Assembly.GetAssembly(typeof(Oxide.Plugins.Timer));
-            var type = assembly.GetType("Oxide.Plugins.Compilation");
-            var method = type.GetMethod(
-                "AddReference",
-                BindingFlags.Instance | BindingFlags.NonPublic,
-                null,
-                new Type[] {typeof(Oxide.Plugins.CompilablePlugin), typeof(AssemblyName)},
-                null);
-            
-            return method;
-        }    
-        
-        [HarmonyTranspiler]
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            return ApplyPatch(instructions, Search, Patch);
-        }
-    }
-    
-    public abstract class AbstractPatchyBoi
-    {
-        static bool CodeInstructionMatch(CodeInstruction a, CodeInstruction b)
-        {
-            if (a.opcode.Name != b.opcode.Name) return false;
-            if (a.operand == b.operand) return true;
-            if (a.operand is MethodBase aMethod && b.operand is MethodBase bMethod)
-                return aMethod.Equals((bMethod));
-            
-            return true;
+            int pos = name.LastIndexOf('_');
+            return pos == -1 ? name : name.Substring(0, pos);
         }
         
-        protected static IEnumerable<CodeInstruction> ApplyPatch(IEnumerable<CodeInstruction> instructions, CodeInstruction[] search, CodeInstruction[] patch)
+        private static void AddHarmonyReference(CompilablePlugin plugin, AssemblyName reference, object __instance)
         {
-            var newInstructions = new List<CodeInstruction>(instructions);
-
-            var found = false;
-            var idx = 0;
-                
-            for (idx = 0; idx < newInstructions.Count - search.Length; idx++)
+            string harmonyModsDir = Path.Combine(Oxide.Core.Interface.Oxide.RootDirectory, "HarmonyMods");
+            if (!Directory.Exists(harmonyModsDir))
             {
-                for (var x = 0; x < search.Length; x++)
-                {
-                    if (!CodeInstructionMatch(newInstructions[idx + x] , search[x]))
-                    {
-                        goto outer;
-                    }
-                }
-
-                // Break out we found it bois
-                found = true;
-                break;
-                
-                outer: ;
+                Debug.LogWarning($"[UnfuckAssemblyLoader] Could not find HarmonyMods directory at {harmonyModsDir}.");
+                return;
             }
-            
-            if (found)
+
+            string assemblyName = GetJukedAssemblyName(reference.Name);
+            string filename = assemblyName + ".dll";
+            // This is such a pain but we have to do it this way because the CompilerFile class is internal.
+            object references = AccessTools.Field(_oxideCompilationClass, "references")?.GetValue(__instance);
+            if (references == null)
             {
-                Debug.Log($"[UnfuckAssemblyLoader] Found injection offset at {idx}");
-                newInstructions.RemoveRange(idx, search.Length);
-                newInstructions.InsertRange(idx, patch);
+                Debug.LogWarning($"[UnfuckAssemblyLoader] Failed to get references field for {plugin.Name}. Compilation may fail.");
             }
             else
             {
-                Debug.Log("[UnfuckAssemblyLoader] Unable to locate injection offset.");
+                Type[] genericAddArgs = { typeof(string), _oxideCompilerFile };
+                var genericDict = typeof(Hash<,>).MakeGenericType(genericAddArgs);
+                var dictAdd = genericDict.GetMethod("Add", new [] { typeof(string), _oxideCompilerFile });
+                
+                if (dictAdd == null)
+                {
+                    Debug.LogWarning($"[UnfuckAssemblyLoader] Failed to get Add method for {plugin.Name} references dictionary. Compilation may fail. This is a bug.");
+                }
+                else
+                {
+                    ConstructorInfo compilerFileConstructor = AccessTools.Constructor(_oxideCompilerFile, new []{ typeof(string), typeof(string) });
+                    object compilerFile = compilerFileConstructor.Invoke(new object[] { harmonyModsDir, filename });
+                    dictAdd.Invoke(references, new[] { filename, compilerFile });
+                }
             }
-            return newInstructions;
-        }            
+            
+            Debug.Log($"[UnfuckAssemblyLoader] Added reference to {filename} for {plugin.Name}.");
+            plugin.References.Add(assemblyName);
+        }
+        
+        public static bool Prefix(CompilablePlugin plugin, string assemblyName, object __instance)
+        {
+            // TODO: Track all this adding of dependencies and references so we can hook harmony.load/unload.
+            try
+            {
+                var assembly = GetHarmonyAssembly(assemblyName);
+                if (assembly == null)
+                {
+                    return true;
+                }
+                
+                AddHarmonyReference(plugin, assembly.GetName(), __instance);
+                
+                foreach (AssemblyName reference in assembly.GetReferencedAssemblies())
+                {
+                    // Janky hardcoded hack copied directly from Oxide :)
+                    if (reference.Name.StartsWith("Newtonsoft.Json") || reference.Name.StartsWith("Rust.Workshop"))
+                    {
+                        continue;
+                    }
+                    
+                    var dep = GetHarmonyAssembly(reference.Name);
+                    if (dep != null)
+                    {
+                        AddHarmonyReference(plugin, reference, __instance);
+                    }
+                    else
+                    {
+                        _internalAddReferenceMethod.Invoke(__instance, new object[] { plugin, reference });
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning($"Exception while trying to AddReference to Oxide plugin: {e}");
+                Debug.LogException(e);
+            }
+
+            return true;
+        }
+
+        // Finds our patch method, but also finds and caches all the various internal/private types we'll need later.
+        public static MethodInfo TargetMethod()
+        {
+            _harmonyLoaderAssemblies = (Dictionary<string, Assembly>)AccessTools.Field(typeof(HarmonyLoader), "assemblyNames")?.GetValue(null);
+            if (_harmonyLoaderAssemblies == null)
+            {
+                Debug.LogWarning("[UnfuckAssemblyLoader] Failed to get HarmonyLoader.assemblyNames");
+                return null;
+            }
+
+            _oxideCompilationClass = AccessTools.TypeByName("Oxide.Plugins.Compilation");
+            if (_oxideCompilationClass == null)
+            {
+                Debug.LogWarning("[UnfuckAssemblyLoader] Unable to get Oxide.Plugins.Compilation.  Is Oxide installed?");
+                return null;
+            }
+            
+            _oxideCompilerFile = AccessTools.TypeByName("ObjectStream.Data.CompilerFile");
+            if (_oxideCompilerFile == null)
+            {
+                Debug.LogWarning("[UnfuckAssemblyLoader] Unable to get ObjectStream.Data.CompilerFile.  Is Oxide installed?");
+                return null;
+            }
+            
+            _internalAddReferenceMethod = AccessTools.Method(_oxideCompilationClass, "AddReference", new []{ typeof(CompilablePlugin), typeof(AssemblyName) });
+            if (_internalAddReferenceMethod == null)
+            {
+                Debug.LogWarning("[UnfuckAssemblyLoader] Unable to get internal Oxide.Plugins.Compilation.AddReference(CompilablePlugin, AssemblyName).  Is Oxide installed?");
+                return null;
+            }
+            
+            var patchMethod = AccessTools.Method(_oxideCompilationClass, "AddReference", new [] { typeof(CompilablePlugin), typeof(string) });
+            if (patchMethod != null) return patchMethod;
+            
+            Debug.LogWarning("[UnfuckAssemblyLoader] Unable to get Oxide.Plugins.Compilation.AddReference(CompilablePlugin, string).  Oxide may have changed.");
+            return null;
+        }
     }
 }
